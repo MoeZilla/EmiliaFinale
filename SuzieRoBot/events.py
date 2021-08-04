5048f22a6522efdd635942ae6180bbdd24bf85a0
Cypher Izumi <85168398+IzumiCypherX@users.noreply.github.com>
2021-08-04 12:55:54 +0530

    Update events.py

--- SuzieRoBot/events.py
+++ SuzieRoBot/events.py
@@ -8,8 +8,8 @@ from pathlib import Path
 from telethon import events
 
 from pymongo import MongoClient
-from YoneRobot import MONGO_DB_URI
-from YoneRobot import telethn
+from SuzieRoBot import MONGO_DB_URI
+from SuzieRoBot import telethn
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI)
@@ -146,7 +146,7 @@ def bot(**args):
     return decorator
 
 
-def YoneRobot(**args):
+def SuzieRoBot(**args):
     pattern = args.get("pattern", None)
     disable_edited = args.get("disable_edited", False)
     ignore_unsafe = args.get("ignore_unsafe", False)
@@ -182,32 +182,32 @@ def load_module(shortname):
         pass
     elif shortname.endswith("_"):
         import importlib
-        import YoneRobot.events
+        import SuzieRoBot.events
 
-        path = Path(f"YoneRobot/modules/{shortname}.py")
-        name = "YoneRobot.modules.{}".format(shortname)
+        path = Path(f"SuzieRoBot/modules/{shortname}.py")
+        name = "SuzieRoBot.modules.{}".format(shortname)
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(mod)
         print("Successfully imported " + shortname)
     else:
         import importlib
-        import YoneRobot.events
+        import SuzieRoBot.events
 
-        path = Path(f"YoneRobot/modules/{shortname}.py")
-        name = "YoneRobot.modules.{}".format(shortname)
+        path = Path(f"SuzieRoBot/modules/{shortname}.py")
+        name = "SuzieRoBot.modules.{}".format(shortname)
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
         mod.register = register
-        mod.YoneRobot = YoneRobot
+        mod.SuzieRoBot = SuzieRoBot
         mod.tbot = telethn
         mod.logger = logging.getLogger(shortname)
         spec.loader.exec_module(mod)
-        sys.modules["YoneRobot.modules." + shortname] = mod
+        sys.modules["SuzieRoBot.modules." + shortname] = mod
         print("Successfully imported " + shortname)
 
 
-path = "YoneRobot/modules/*.py"
+path = "SuzieRoBot/modules/*.py"
 files = glob.glob(path)
 for name in files:
     with open(name) as f:
